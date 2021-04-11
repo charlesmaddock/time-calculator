4745cca82f416cabd5d2cd8774ad2eb2a262c62b
Charles <charlesalexander.maddock@gmail.com>
2021-04-11 21:31:37 +0200

    hour error bug fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // The important thing is that there is a spacebar between the intervals and a hyphen
 // the start and the stop time.
 let times =
-  "09:17-12:26 13:03-16:15 09:45-12:25 12:45-13:05 13:34-15:05 10:36-11:57 13:20-17:04 15:03-17:30 18:41-20:30 08:55-13:35 14:12-17:20 09:08-12:00 13:22-15:40 13:07-14:25 12:20-16:45";
+  "08:40-12:20 19:30-20:30 12:15-15:50 09:20-12:00 13:50-14:30 09:00-11:45 12:30-14:15 12:07-12:30 15:17-17:33 09:56-12:05 13:00-14:20 16:02-17:05 17:37-17:51";
 
 console.log(
   "Check the logged values below to make sure you didn't format your work time intervals incorrectly!\n"
@@ -21,7 +21,7 @@ for (i = 0; i < timesArray.length; i++) {
     endTime.split(":")[0],
     endTime.split(":")[1]
   );
-  var hourDiff = new Date(endDate - startDate).getHours();
+  var hourDiff = new Date(endDate - startDate).getHours() - 1;
   var minDiff = new Date(endDate - startDate).getMinutes();
   totalHours += hourDiff;
   totalHours += minDiff / 60;
